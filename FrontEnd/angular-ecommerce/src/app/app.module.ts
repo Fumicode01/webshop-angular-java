70bc580a4f5a653fd76272c00f22e400064022dc
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-02 11:21:34 +1100

    Added FrontEnd code for search products by keyword

--- FrontEnd/angular-ecommerce/src/app/app.module.ts
+++ FrontEnd/angular-ecommerce/src/app/app.module.ts
@@ -7,14 +7,18 @@ import {HttpClientModule} from '@angular/common/http';
 import { ProductService } from './services/product.service';
 import { Routes, RouterModule } from "@angular/router";
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { SearchComponent } from './components/search/search.component';
 
 
 const routes: Routes = [
+    {path: "search/:keyword", component: ProductListComponent},
+
     {path: "category/:id", component: ProductListComponent},
     {path: "category", component: ProductListComponent},
     {path: "products", component: ProductListComponent},
     {path: "", redirectTo: "/priducts", pathMatch: "full"},
     {path: "**", redirectTo: "/priducts", pathMatch: "full"},
+
 
 ]
 
@@ -23,7 +27,8 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductCategoryMenuComponent
+    ProductCategoryMenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
--- FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/app/common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -11,34 +11,62 @@ import { ProductService } from 'src/app/services/product.service';
 export class ProductListComponent implements OnInit {
 
   products: Product[];
-  currentCategoryId:number;
+  currentCategoryId: number;
+  searchMode: boolean;
+
   constructor(private productService: ProductService,
-                        private route: ActivatedRoute) { 
-      this.products = [];
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
-  ngOnInit(): void {
-      this.route.paramMap.subscribe(()=>{
-           this.listProducts();
-      })
-     
-  }
+  listProducts() {
 
-  listProducts(){
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    const hasCategoryId: boolean = this.route.snapshot.paramMap.has("id");
-
-    if(hasCategoryId){
-        this.currentCategoryId = +this.route.snapshot.paramMap.get("id")
-    } else {
-        this.currentCategoryId = 1;
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    }
+    else {
+      this.handleListProducts();
     }
 
-      this.productService.getProductList(this.currentCategoryId).subscribe(
-          data => {
-              this.products = data;
-          }
-      )
   }
 
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    // now search for the products using keyword
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    )
+  }
+
+  handleListProducts() {
+
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id" param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else {
+      // not category id available ... default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data => {
+        this.products = data;
+      }
+    )    
+  }
 }
