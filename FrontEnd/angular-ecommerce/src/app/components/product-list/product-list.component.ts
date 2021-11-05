362a2fbac79b782f8c353d42aa188c08a76e8b7f
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-05 22:47:47 +1100

    Add cartService to add items and calculate totalPrice

--- FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ FrontEnd/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from 'src/app/common/product';
 import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { CartItem } from 'src/app/common/cart-item';
 
 @Component({
   selector: 'app-product-list',
@@ -23,7 +25,8 @@ export class ProductListComponent implements OnInit {
 
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) { }
+                     private cartService: CartService,
+                     private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -113,6 +116,11 @@ export class ProductListComponent implements OnInit {
   addToCart(theProduct: Product){
 
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`)
+
+    const theCartItem = new CartItem(theProduct);
+    this.cartService.addToCart(theCartItem);
+
+
 
   }
 }
