6d0f3bc869e15d2e42d583bb10ee2d27b1d5b4d9
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-16 00:32:48 +1100

    Added Login Component with Sign-in Widget

--- FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
+++ FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,5 +1,5 @@
 export default {
-    oicd:{
+    oidc:{
         clientId:'0oa2mwyy9176vj1dg5d7',
         issuer:'https://dev-11899557.okta.com/oauth2/default',
         redirectUrl: 'http://localhost:4200/login/callback',
