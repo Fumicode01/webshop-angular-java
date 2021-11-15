83e80942ebb4d2aeddf7b528f4c5317c6b13df0f
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-15 19:55:20 +1100

    Add config to Okta and dependency
    Applied Css into angular.json

---
+++ FrontEnd/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+    oicd:{
+        clientId:'0oa2mwyy9176vj1dg5d7',
+        issuer:'https://dev-11899557.okta.com/oauth2/default',
+        redirectUrl: 'http://localhost:4200/login/callback',
+        scopes:['openid', 'profile', 'email']
+
+    }
+}
