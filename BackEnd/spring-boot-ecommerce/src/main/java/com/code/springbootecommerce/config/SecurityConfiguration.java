358891e2ff3599acb5421a3a95050319f389b535
Fumihito <65881354+Fumicode01@users.noreply.github.com>
2021-11-17 17:42:39 +1100

    Added Interceptor to Frontend code
    created Interceptor in services
    handle acceccToken to implement to Request Header

--- BackEnd/spring-boot-ecommerce/src/main/java/com/code/springbootecommerce/config/SecurityConfiguration.java
+++ BackEnd/spring-boot-ecommerce/src/main/java/com/code/springbootecommerce/config/SecurityConfiguration.java
@@ -24,6 +24,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         // force a non-empty response body for 401's to make the response more friendly
         Okta.configureResourceServer401ResponseBody(http);
+
+        
     }
 }
 
